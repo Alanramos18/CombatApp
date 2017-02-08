60228f1a697b12727387488573446f8de6ca6a25
Alanramos18 <alanramos_18@hotmail.com>
2017-02-08 03:12:52 -0300

    VS proyect

---
+++ CombatApp/CombatApp/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatApp
+{
+    class Hero : Stats
+    {
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public int Gold { get; set; }
+
+        public Hero()
+        {
+            Health = 100;
+            Mana = 50;
+            Experience = 0;
+            Gold = 0;
+            Level = 1;
+        }
+
+        private void checkExp()
+        {
+            int nextExpTarget = (Level * 100) + (5 * Level);
+            if (Experience >= nextExpTarget)
+                levelUp();
+        }
+
+        public void levelUp()
+        {
+            Health += 18;
+            Mana += 8;
+            Level++;
+            Experience = 0;
+        }
+    }
+}
